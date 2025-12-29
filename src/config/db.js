99fef1bfa10e2d18f235a08ceec4a8714bec3ee7
MohammadReza Akbari Sheikh Abad <mohammadreza.akbarisheikhabad@email.kntu.ac.ir>
2025-12-29 15:25:15 +0330

    complete auth.controller

--- src/config/db.js
+++ src/config/db.js
@@ -6,3 +6,5 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+module.exports = pool
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+const pool = require('../config/db')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const [exiting] = pool.query(
+        'select id from User where email=?',
+        [email]
+    );
+
+    if (exiting.length) {
+        res.status(400).json({ message: 'Email already exists' })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await pool.query(
+        'insert into User(name, email, password) values (?, ?, ?)',
+        [name, email, hashedPassword]
+    )
+
+    const token = jwt.sign(
+        { id: result.insertId, email },
+        process.env.JWT_SECRET
+    )
+    res.json({ msg: 'registered ', token: token })
+}
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const [users] = pool.query(
+        'select * from User where email=?',
+        [email]
+    )
+
+    if (!user.length) {
+        res.status(400).json({ msg: 'Invalid email' })
+    }
+
+    const user = users[0]
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        res.status(400).json({ msg: 'Invalid paswword' })
+    }
+
+    const token = jwt.sign(
+        { id: user.id, email },
+        process.env.JWT_SECRET
+    );
+
+    res.json({ msg: 'logined ', token: token });
+}
