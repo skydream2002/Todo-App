d2282581079d6fa4e6789f2afdd3663e00fd7bde
MohammadReza Akbari Sheikh Abad <mohammadreza.akbarisheikhabad@email.kntu.ac.ir>
2025-12-29 14:48:33 +0330

    completing routes

---
+++ src/routes/todo.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {createTodo, getTodo, updateTodo, deleteTodo} = require('../controllers/todo.controller');
+const auth = require('../middlewares/auth.middleware')
+
+const router = express.Router()
+
+router.post('/', auth, createTodo)
+router.post('/', auth, getTodo)
+router.post('/:id', auth, updateTodo)
+router.post('/:id', auth, deleteTodo)
+
+module.exports = router;
