e650dead6adb7ab8f20c53bc605ddc573bd799f8
MohammadReza Akbari Sheikh Abad <mohammadreza.akbarisheikhabad@email.kntu.ac.ir>
2025-12-29 19:06:33 +0330

    fixed some bugs on auth token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,17 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ msg: 'Unauthorized' })
+    if (!authHeader) {
+        return res.status(401).json({ msg: 'empty token' });
     }
+
+    const parts = authHeader.split(' ');
+
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        return res.status(401).json({ msg: 'Invalid auth header format' });
+    }
+
+    const token = parts[1];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
-    } catch {
-        res.status(401).json({ msg: 'Unauthorized' })
+    } catch (err) {
+        return res.status(401).json({ msg: 'Unauthorized' });
     }
 };
